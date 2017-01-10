ae8f7f7d3425282c5216ab71fa9d4de25c4f3066
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 10:23:48 +0530

    Create CheckNum.java

---
+++ CheckNum.java
@@ -0,0 +1,35 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String... lk) {
+		
+		
+
+
+		
+		System.out.println("Enter Your Number");
+		Scanner sc=new Scanner(System.in); 	
+		
+		try{
+			
+		
+		Long l=sc.nextLong();
+		Double d=sc.nextDouble();
+		System.out.println("true");
+		}
+		catch(Exception e){
+			System.err.println("false");
+		}
+		
+		
+		
+		}
+		
+
+	}
+
+
